097b4f79a4b4aae37f4926a6fbaf77752bfcac78
Manuel De Jesus Baez Batista <manuelbaezbatista@gmail.com>
2021-04-19 22:55:42 -0400

    Add improved error message

--- index.js
+++ index.js
@@ -9,10 +9,14 @@ const { _: username, token } = yargs(hideBin(process.argv)).argv
 const fetchContributions = require('./fetch-contributions')
 
 if (!token || typeof token !== 'string') {
-  console.error(chalk.red('No access was token provided.'))
+  console.error(
+    chalk.red(`
+      No github access token was provided:
+        An access token is needed to access the contributor's information.
+  `)
+  )
   process.exit(1)
 }
-
 fetchContributions(token, username).then((contributionCount) => {
   console.log(chalk.green(contributionCount))
 })
